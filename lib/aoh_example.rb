3221a86dc4e5a90ca4537a810cae2d4868948967
Osgood Gunawan <osgoodgunawan@hotmail.com>
2020-01-16 08:03:21 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -11,8 +11,6 @@ def assembled_aoh
 end
 
 def literal_aoh
-  # Using Array literal syntax only, build a nested array that uses the data in
-  # held in the constants
   result=[{ name:  "Don Gately", occupation: "Live-in Staff" },
   { name:  "Joelle van Dyne", occupation: "Radio Personality" },
 { name:  "Pat Monteseian", occupation: "Staff" },
@@ -22,6 +20,8 @@ def literal_aoh
 end
 
 def aoh_lookup(aoh, row, key)
+  
+  
 end
 
 def aoh_update(aoh, row, key, new_value)
